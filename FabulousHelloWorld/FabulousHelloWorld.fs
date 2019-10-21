55fe801c25ec84709ef8ef35dedb8377f0dcad0c
Martin Rodriguez <ixmrm01@gmail.com>
2019-10-20 21:48:55 -0500

    Add files via upload

---
+++ FabulousHelloWorld/FabulousHelloWorld.fs
@@ -0,0 +1,93 @@
+﻿// Copyright 2018-2019 Fabulous contributors. See LICENSE.md for license.
+namespace FabulousHelloWorld
+
+open System.Diagnostics
+open Fabulous
+open Fabulous.XamarinForms
+open Fabulous.XamarinForms.LiveUpdate
+open Xamarin.Forms
+
+module App = 
+    type Model = string
+
+    type Msg = 
+        | UpdateMessage of string
+
+    let initModel = ""
+
+    let init () = 
+        initModel, Cmd.ofMsg(UpdateMessage "World!")
+
+    let update msg model =
+        match msg with
+        | UpdateMessage s -> sprintf "Hello %s" s, Cmd.none
+
+    let view (model: Model) dispatch =
+        View.ContentPage(
+            content = View.StackLayout(
+                padding = 20.0,
+                verticalOptions = LayoutOptions.Center,
+                children = [
+                    View.Label(
+                        text = model,
+                        horizontalOptions = LayoutOptions.Center,
+                        widthRequest = 200.0,
+                        horizontalTextAlignment=TextAlignment.Center
+                    )
+                ]
+            )
+        )
+
+    // Note, this declaration is needed if you enable LiveUpdate
+    let program = Program.mkProgram init update view
+
+type App () as app = 
+    inherit Application ()
+
+    let runner = 
+        App.program
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> XamarinFormsProgram.run app
+
+#if DEBUG
+    // Uncomment this line to enable live update in debug mode. 
+    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/tools.html#live-update for further  instructions.
+    //
+    //do runner.EnableLiveUpdate()
+#endif    
+
+    // Uncomment this code to save the application state to app.Properties using Newtonsoft.Json
+    // See https://fsprojects.github.io/Fabulous/Fabulous.XamarinForms/models.html#saving-application-state for further  instructions.
+#if APPSAVE
+    let modelId = "model"
+    override __.OnSleep() = 
+
+        let json = Newtonsoft.Json.JsonConvert.SerializeObject(runner.CurrentModel)
+        Console.WriteLine("OnSleep: saving model into app.Properties, json = {0}", json)
+
+        app.Properties.[modelId] <- json
+
+    override __.OnResume() = 
+        Console.WriteLine "OnResume: checking for model in app.Properties"
+        try 
+            match app.Properties.TryGetValue modelId with
+            | true, (:? string as json) -> 
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, json = {0}", json)
+                let model = Newtonsoft.Json.JsonConvert.DeserializeObject<App.Model>(json)
+
+                Console.WriteLine("OnResume: restoring model from app.Properties, model = {0}", (sprintf "%0A" model))
+                runner.SetCurrentModel (model, Cmd.none)
+
+            | _ -> ()
+        with ex -> 
+            App.program.onError("Error while restoring model found in app.Properties", ex)
+
+    override this.OnStart() = 
+        Console.WriteLine "OnStart: using same logic as OnResume()"
+        this.OnResume()
+#endif
+
+
